295914ba7bc9ba239d5f8bbee9edcbe0ed00600b
FatmaSorour8 <fatmamohamed862s@outlook.com>
2024-05-25 23:10:09 +0300

    filter products according to categoryName and refactor some designs

--- ClientSide/src/app/Pages/products/products.component.ts
+++ ClientSide/src/app/Pages/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../../interfaces/i-product';
+import { ICategory } from '../../interfaces/i-category';
 import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ProductItemComponent } from './product-item/product-item.component';
@@ -10,23 +12,52 @@ import { ProductItemComponent } from './product-item/product-item.component';
   standalone: true,
   imports: [CommonModule, RouterLink, ProductItemComponent],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: IProduct[];
+  filteredProducts: IProduct[];
+  categories: ICategory[];
+  activeCategory: string;
 
-  products:IProduct[];
-  constructor(public productService: ProductService) { 
+  constructor(private productService: ProductService, private categoryService: CategoryService) {
     this.products = [];
+    this.filteredProducts = [];
+    this.categories = [];
+    this.activeCategory = 'All';
   }
 
-  ngOnInit():void {
+  ngOnInit(): void {
     this.productService.getAll().subscribe({
       next: (data) => {
         this.products = data;
+        this.filterProducts('All');
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+
+    this.categoryService.getAll().subscribe({
+      next: (data) => {
+        this.categories = data;
       },
       error: (error) => {
         console.log(error);
       }
     });
   }
- }
+
+  filterProducts(category: string): void {
+    this.activeCategory = category;
+    if (category === 'All') {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(product => product.categoryName === category);
+    }
+  }
+
+  trackByProductId(index: number, product: IProduct): number {
+    return product.id;
+  }
+}
