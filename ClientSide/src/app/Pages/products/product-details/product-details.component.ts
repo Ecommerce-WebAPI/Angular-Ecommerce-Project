4b4983fcbcb6e8cb4a20102231e9f271088a9160
FatmaSorour8 <fatmamohamed862s@outlook.com>
2024-05-26 19:28:49 +0300

    Show Related Products in Products Details Page

--- ClientSide/src/app/Pages/products/product-details/product-details.component.ts
+++ ClientSide/src/app/Pages/products/product-details/product-details.component.ts
@@ -1,33 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ProductService } from '../../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { IProduct } from '../../../interfaces/i-product';
+import { ProductItemComponent } from "../product-item/product-item.component";
 
 @Component({
-  selector: 'app-product-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+    selector: 'app-product-details',
+    standalone: true,
+    templateUrl: './product-details.component.html',
+    styleUrls: ['./product-details.component.css'],
+    imports: [CommonModule, FormsModule, CarouselModule, RouterLink, ProductItemComponent]
 })
+
 export class ProductDetailsComponent implements OnInit {
   productId: any;
   product?: any;
+  quantity: number = 1;
+  relatedProducts: IProduct[] = [];
+  currentCategory = '';
+
+
+  
   constructor(
     public activatedRoute: ActivatedRoute,
     private productService: ProductService,
     public router: Router
   ) {}
+
   ngOnInit(): void {
     this.productId = this.activatedRoute.snapshot.params['id'];
     this.productService.getById(this.productId).subscribe({
       next: (data) => {
         this.product = data;
+        this.fetchRelatedProducts(this.product.categoryName);
       },
       error: (error) => {
         console.log(error);
       }
-  });
-    console.log(this.product);
+    });
+  }
+
+  fetchRelatedProducts(category: string): void {
+    this.currentCategory = category;
+    this.productService.getByCategory(category).subscribe({
+      next: (data) => {
+        this.relatedProducts = data.filter(p => p.id !== this.productId);
+        this.relatedProducts = data.filter(product => product.categoryName === category);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  incrementQuantity() {
+    this.quantity++;
   }
 }
-
